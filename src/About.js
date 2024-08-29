f3cff731be5ec9230fda45b51d4be23acda797b1
Jack Stoneman <jackstoneman@Jacks-Air.ip>
2024-08-29 21:37:57 +1200

    incomplete changes

--- src/About.js
+++ src/About.js
@@ -1,26 +1,45 @@
 import "./About.css";
 import aboutPhoto from "./assets/gallery-1.jpg";
+import { useState } from "react";
 
 function About() {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div id="about" className="section">
       <div className="section-header">ABOUT ME</div>
       <div className="about-content">
         <img src={aboutPhoto} className="about-photo" />
-        <div className="about-paragraph">
-          I’ve been a sports enthusiast all my life and have dedicated many
-          years to strength training, high-altitude trekking and living a fit
-          and healthy lifestyle. I spent 3 years in the military, which
-          instilled much discipline in my early life which I now cultivate
-          through personal training and nutrition coaching. I hold a
-          post-graduate degree from Oxford University and a Bachelor of Science
-          from University of Kansas.
-          <br />
-          <br />
-          My philosophy on keeping fit and eating right – this should become
-          your lifestyle. If it isn’t, then make it your lifestyle. Find what
-          you enjoy with fitness, find the balance and stick with that plan.
-          Sustainability is so crucial for achieving long-term resilience.
+        <div className="about-text">
+          <div className={"about-abbreviated" + (expanded ? " hidden" : "")}>
+            This is a placeholder sentence for the shortened about section.
+            <br />
+            This is another placeholder sentence, the real content will come
+            soon!
+          </div>
+          <div className={"about-paragraph" + (expanded ? "" : " hidden")}>
+            I’ve been a sports enthusiast all my life and have dedicated many
+            years to strength training, high-altitude trekking and living a fit
+            and healthy lifestyle. I spent 3 years in the military, which
+            instilled much discipline in my early life which I now cultivate
+            through personal training and nutrition coaching. I hold a
+            post-graduate degree from Oxford University and a Bachelor of
+            Science from University of Kansas.
+            <br />
+            <br />
+            My philosophy on keeping fit and eating right – this should become
+            your lifestyle. If it isn’t, then make it your lifestyle. Find what
+            you enjoy with fitness, find the balance and stick with that plan.
+            Sustainability is so crucial for achieving long-term resilience.
+          </div>
+          <button
+            className="expand"
+            onClick={() => {
+              setExpanded(!expanded);
+            }}
+          >
+            Learn more about me
+          </button>
         </div>
       </div>
     </div>
--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,79 @@
 import "./Contact.css";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+const formSuccessMessage =
+  "Thanks for your submission! I'll get back to you as soon as possible.";
+const formErrorMessage =
+  "Oh no! Something went wrong with your submission. Please try again later.";
 
 function Contact() {
+  const form = useRef();
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_scerlce",
+        "template_vk45st4",
+        form.current,
+        "_C3IbivUsbi6vVrI8"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setModalMessage(formSuccessMessage);
+          setIsOpen(true);
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          setModalMessage(formErrorMessage);
+          setIsOpen(true);
+        }
+      );
+  };
+
   return (
     <div id="contact" className="section blue">
       <div className="section-header">ENQUIRE NOW</div>
+      <div className="contact-form">
+        <div className="contact-form-title">Get in touch</div>
+        <div className="form-wrapper">
+          <form ref={form} onSubmit={sendEmail}>
+            <input
+              type="text"
+              name="user_name"
+              placeholder="Your name"
+              required
+            />
+            <input
+              type="email"
+              name="user_email"
+              placeholder="Your email"
+              required
+            />
+            <input
+              type="text"
+              name="user_gender"
+              placeholder="Your gender"
+              required
+            />
+            <input
+              type="text"
+              name="user_phone"
+              placeholder="Your phone"
+              required
+            />
+            <textarea name="message" placeholder="Message" required />
+            <input className="button purple" type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
