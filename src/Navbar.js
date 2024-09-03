2a93cd854293e8238dd0b5bb3a9af579a7c74bb5
Jack Stoneman <jackstoneman@Jacks-Air.ip>
2024-09-03 18:20:54 +1200

    Incomplete navbar suboption implementation

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,12 +4,25 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="inner-wrapper">
-        <div className="navbar-title">Passion For Fitness NZ</div>
+        <a className="navbar-title" href="#welcome">
+          Passion For Fitness NZ
+        </a>
         <div className="navbar-links">
-          <a href="#welcome">Welcome</a>
-          <a href="#calendar">Contact</a>
           <a href="#about">About Me</a>
-          <a href="#process">My Process</a>
+          <a href="#process">
+            Training Types
+            <div className="sub-options">
+              <a href="">1:1 Training</a>
+              <a href="">Online Training</a>
+            </div>
+          </a>
+          <a href="#calendar">
+            Contact
+            <div className="sub-options">
+              <a href="">Book a Call</a>
+              <a href="">Enquire Now</a>
+            </div>
+          </a>
         </div>
       </div>
     </div>
