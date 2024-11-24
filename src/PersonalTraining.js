756368c984635ba6683793dc71bf8c8973ec5657
Jack Stoneman <jackstoneman@Jacks-MacBook-Air.local>
2024-11-25 07:43:23 +1300

    WIP

--- src/PersonalTraining.js
+++ src/PersonalTraining.js
@@ -1,16 +1,28 @@
-import "./PersonalTraining.css"
+import "./PersonalTraining.css";
 import aboutPhoto from "./assets/gallery-1.jpg";
 
 function PersonalTraining() {
-    return(<div id="personalTraining" className="section blue">
-        <div className="section-header">1:1 TRAINING</div>
-        <div className="training-type">
-            <div className="training-text">
-                This section will talk about 1:1 training and the benefits etc...
-            </div>
-            <img src={aboutPhoto} className="training-photo" />
+  return (
+    <div id="personalTraining" className="section blue">
+      <div className="section-header">1:1 TRAINING</div>
+      <div className="pt-section">
+        <div className="bold">
+          Achieve real results with a proven program tailored just for you
         </div>
-    </div>)
+        <div>
+          Transform your body and reach your fitness goals with my personalised
+          12-16 week body transformation program, now open to members AND
+          non-members of SnapFitness gyms in Christchurch. Whether you're
+          looking to lose weight, build muscle, or boost your strength, this
+          all-inclusive program provides the tools, support, and flexibility you
+          need.
+        </div>
+      </div>
+      <div className="pricing-container">
+        <div className="offset-border"></div>
+      </div>
+    </div>
+  );
 }
 
 export default PersonalTraining;
--- src/TestimonialCarousel.js
+++ src/TestimonialCarousel.js
@@ -2,26 +2,63 @@ import "./TestimonialCarousel.css";
 import Testimonial from "./Testimonial";
 import testimonials from "./assets/testimonials.json";
 import React, { useState } from "react";
+import _ from "lodash";
+
+const maxWidth = (testimonials.length - 1) * 360;
 
 function TestimonialCarousel() {
   const [currentIndex, setCurrentIndex] = useState(testimonials.length / 2);
 
-  // Function to go to the next testimonial
-  const nextTestimonial = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+  let sliderTimeout;
+
+  const updatePositions = (dir) => {
+    let carousel = document.querySelector("#testimonials .carousel");
+    Array.from(carousel.children).map((child, index) => {
+      let style = getComputedStyle(child),
+        matrix = new DOMMatrix(style.transform),
+        translateX = matrix.m41,
+        basePosition = parseInt(index) * 360,
+        newTranslation;
+
+      if (dir) {
+        if (basePosition + translateX > 0) {
+          newTranslation = translateX - 360;
+        } else {
+          newTranslation = maxWidth - index * 360;
+        }
+        child.style.transform = `translateX(${newTranslation}px)`;
+      } else {
+        if (basePosition + translateX < maxWidth) {
+          newTranslation = translateX + 360;
+        } else {
+          newTranslation = -1 * basePosition;
+        }
+        child.style.transform = `translateX(${newTranslation}px)`;
+      }
+    });
   };
 
+  // Function to go to the next testimonial
+  const nextTestimonial = _.debounce(() => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
+    updatePositions(1);
+  }, 300);
+
   // Function to go to the previous testimonial
-  const prevTestimonial = () => {
+  const prevTestimonial = _.debounce(() => {
     setCurrentIndex(
       (prevIndex) => (prevIndex - 1 + testimonials.length) % testimonials.length
     );
-  };
+    updatePositions(0);
+  }, 300);
 
   return (
     <div id="testimonials" className="section blue">
       <div className="section-header">TESTIMONIALS</div>
-      <div className="carousel-wrapper">
+      <div
+        className="carousel-wrapper"
+        style={{ maxWidth: `${maxWidth + 460}px` }}
+      >
         <span className="arrow prev" onClick={prevTestimonial} />
         <div className="carousel">
           {testimonials.map((testimonial, index) => {
@@ -32,11 +69,6 @@ function TestimonialCarousel() {
                 className={`testimonial ${
                   offset === 0 ? "center" : offset > 0 ? "right" : "left"
                 }`}
-                style={{
-                  transform: ` translateX(${
-                    (0 - currentIndex) * 320
-                  }px) scale(${offset === 0 ? 1.0 : 0.8})`, // Center testimonial is bigger
-                }}
               >
                 <div className="testimonial-header">
                   <div className="testimonial-name">{testimonial.name}</div>
